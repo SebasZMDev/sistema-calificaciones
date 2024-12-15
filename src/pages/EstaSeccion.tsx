17b92d3cbf5def368ea3d7b012eae9ece545f905
sebaszm <sebaszelada28@gmail.com>
2024-12-14 22:20:22 -0500

    final-push-maybe

--- src/pages/EstaSeccion.tsx
+++ src/pages/EstaSeccion.tsx
@@ -2,11 +2,16 @@ import { useNavigate } from "react-router-dom";
 import "../styles/EstaSeccionS.css";
 import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
-import AlumnoInfo from "../components/AlumnoInfo";
+
 
 const EstaSeccion = () => {
   const [esteGrado, setEsteGrado] = useState("");
+  const [alumnoInfo, setAlumnoInfo] = useState(false);
+  const [editarAlumno, setEditarAlumno] = useState(false);
+  const [verLibreta, setVerLibreta] = useState(false);
   const navigate = useNavigate();
+
+
 
   const data = [
     {
@@ -215,6 +220,7 @@ const EstaSeccion = () => {
           <button onClick={IrASecciones}>Volver</button>
           <button onClick={()=>setListado(true)}>Generar Reportes</button>
           <button>Guardar</button>
+          <button>Seleccionar Todo</button>
           <button>Enviar</button>
         </div>
 
@@ -229,8 +235,8 @@ const EstaSeccion = () => {
               <th>Grado</th>
               <th>Sección</th>
               <th>Promedio General</th>
-              <th>Envio Formato</th>
               <th>Seleccionar</th>
+              <th>Formato</th>
             </tr>
           </thead>
           <tbody>
@@ -241,17 +247,17 @@ const EstaSeccion = () => {
               const randomNumber = Math.floor(Math.random() * (20 - 13 + 1)) + 13;
               return (
                 <tr key={index}>
-                  <td>{row.id}</td>
-                  <td>{row.dni}</td>
-                  <td onClick={()=>''}>{row.nombreCompleto}</td>
-                  <td>{row.nivel}</td>
-                  <td>{esteGrado}</td>
-                  <td>{randomValue}</td>
-                  <td>{Listado?randomNumber:(<img style={{width:"40px", height:"40px"}} src="../.././public/media/advertencia.png"/>)}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{row.id}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{row.dni}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{row.nombreCompleto}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{row.nivel}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{esteGrado}</td>
+                  <td onClick={()=>setAlumnoInfo(true)}>{randomValue}</td>
+                  <td>{Listado?randomNumber:(<img style={{width:"30px", height:"30px"}} src="../.././public/media/advertencia.png"/>)}</td>
                   <td><input disabled={Listado?false:true} style={{width:"30px", height:"30px"}} type="checkbox"/></td>
                   <td>
                     <button className="btn-pdf">PDF</button>
-                    <button>Ver</button>
+                    <button onClick={()=>setVerLibreta(true)}>Ver</button>
                   </td>
                 </tr>
               );
@@ -259,7 +265,125 @@ const EstaSeccion = () => {
           </tbody>
         </table>
       </div>
-      <AlumnoInfo/>
+      {alumnoInfo?(
+                <div className='af-general-container'>
+                <div className='af-background'></div>
+                <div className="af-container">
+                    <img src="../.././public/media/default.jpg" alt="Foto del alumno" className="af-foto" />
+                      {editarAlumno?
+                      (
+                        <div className="af-info">
+                        <input style={{fontFamily:"Nunito, serif", fontSize:"1em", marginLeft:"20px"}} value="Alegre Jesus Ninaquiste"/>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Código:</span><input value="i20236969"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>DNI:</span><input value="12345678"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Nivel:</span><input value="Primaria"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Grado:</span><input value="3ro"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Sección:</span><input value="C"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Apoderado:</span><input value="Padre Alegre"/></div>
+                        <div><span style={{fontFamily:"Nunito, serif", fontSize:"0.8em"}}>Contacto:</span><input value="soycremacorazon@hotmail.com"/></div>
+                        <div className="af-actions">
+                           <button className="af-button af-edit"  onClick={()=>setEditarAlumno(true)}>Guardar</button>
+                           <button className="af-button af-close" onClick={()=>setAlumnoInfo(false)}>Cerrar</button>
+                        </div>
+                       </div>
+                      )
+                      :(
+                        <div className="af-info">
+                          <h2>Jesus Alegre Ninaquispe</h2>
+                          <p><strong>Código:</strong> i20236969</p>
+                          <p><strong>DNI:</strong> 12345678</p>
+                          <p><strong>Nivel:</strong> Primaria</p>
+                          <p><strong>Grado:</strong> 3ro</p>
+                          <p><strong>Sección:</strong> C</p>
+                          <p><strong>Apoderado:</strong> Padre Alegre</p>
+                          <p><strong>Contacto:</strong> soycremacorazon@hotmail.com</p>
+                          <div className="af-actions">
+                             <button className="af-button af-edit"  onClick={()=>setEditarAlumno(false)}>Editar</button>
+                             <button className="af-button af-close" onClick={()=>setAlumnoInfo(false)}>Cerrar</button>
+                          </div>
+                         </div>
+                      )}
+
+                </div>
+            </div>
+      ):(null)}
+
+
+{verLibreta ? (
+  <div className='af-general-container'>
+    <div className='af-background'></div>
+    <div className="lib-container">
+      <div>
+        <h2 className="af-title">Libreta de Calificaciones</h2>
+
+        <div className="af-student-name">
+          <strong>Nombre del Alumno:</strong> Jesus Alegre Ninaquispe
+        </div>
+
+        <div className="af-grade-table">
+          <table>
+            <thead>
+              <tr>
+                <th>Curso</th>
+                <th>T1</th>
+                <th>T2</th>
+                <th>T3</th>
+                <th>EF</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Matemáticas</td>
+                <td>15</td>
+                <td>16</td>
+                <td>18</td>
+                <td>19</td>
+              </tr>
+              <tr>
+                <td>Historia</td>
+                <td>12</td>
+                <td>14</td>
+                <td>15</td>
+                <td>17</td>
+              </tr>
+              <tr>
+                <td>Ciencias</td>
+                <td>14</td>
+                <td>13</td>
+                <td>15</td>
+                <td>18</td>
+              </tr>
+              <tr>
+                <td>Lenguaje</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+              </tr>
+              <tr>
+                <td>Arte</td>
+                <td>16</td>
+                <td>15</td>
+                <td>18</td>
+                <td>19</td>
+              </tr>
+              <tr>
+                <td>Educación Física</td>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+                <td>20</td>
+              </tr>
+            </tbody>
+          </table>
+          <div style={{width:"100%",height:"50px", display:"grid",alignItems:"center", justifyContent:"center"}}>
+            <button onClick={()=>setVerLibreta(false)} style={{backgroundColor:"crimson", transform:"scale(1.3)"}}>Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+) : null}
     </>
   );
 };
